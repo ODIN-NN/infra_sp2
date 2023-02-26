4fa39e437004e103d3ea71cacd4d9bb1ea816cab
Alexander_Odintsov <odintsov.alexander.nn@yandex.com>
2023-02-26 19:19:32 +0300

    infra_sp2_try1

---
+++ api_yamdb/api_yamdb/urls.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+from rest_framework import routers
+
+from api.views import (
+    CategoryViewSet,
+    GenreViewSet,
+    TitleViewSet,
+    UserViewSet,
+    get_token,
+    create,
+    ReviewViewSet,
+    CommentViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register(
+    r'categories',
+    CategoryViewSet,
+    basename='categories'
+)
+router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet, basename='titles')
+router.register('users', UserViewSet, basename='users')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/auth/token/', get_token),
+    path('api/v1/auth/signup/', create),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
+]
